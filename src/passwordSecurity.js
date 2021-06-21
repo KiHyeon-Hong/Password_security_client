923f9ee260698629f6bbf4e16f5524b5bfe932b4
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-21 21:25:11 +0900

    model test

--- src/passwordSecurity.js
+++ src/passwordSecurity.js
@@ -1,12 +1,31 @@
 const fs = require('fs');
+const http = require('http');
+
+var tf = require('@tensorflow/tfjs');
+require("tfjs-node-save");
 
 class passwordSecurity {
     passwordValidation(password) {
+        const test = async function() {
+            const loadedModel = await tf.loadLayersModel("file://" + __dirname + "/../passwordModel/model.json");
+            loadedModel.predict(tf.tensor([[0, 2, 10]])).print();
+        }
         
+        test();
         return 'passwordValidation';
     };
 
     passwordModelDistribution(versionData, comment) {
+        const weights = fs.createWriteStream("./PasswordSecurityClient/passwordModel/weights.bin");
+        http.get("http://localhost:65001/passwordModelDistributionWeight", function(response) {
+            response.pipe(weights);
+        });
+
+        const model = fs.createWriteStream("./PasswordSecurityClient/passwordModel/model.json");
+        http.get("http://localhost:65001/passwordModelDistributionModel", function(response) {
+            response.pipe(model);
+        });
+
         return 'passwordModelDistribution';
     };
 
