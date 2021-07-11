502f0585a87c8ed13946b6b6b53677d5c21ef5de
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-07-11 13:22:13 +0900

    Update PasswordSecurityCheck.js

--- src/PasswordSecurityCheck.js
+++ src/PasswordSecurityCheck.js
@@ -9,6 +9,14 @@ const comparePoint = new koreanZxcvbnString.koreanZxcvbnString.koreanZxcvbnStrin
 
 class PasswordSecurityCheck {
     passwordSecurityCheck(password, predictPoint) {
+
+        var zxcvbnPoint = parseInt(parseInt(((koreanZxcvbn(password).score * 2) + comparePoint.frequencyComparePoint(password))) / 2) < 5? parseInt(parseInt(((koreanZxcvbn(password).score * 2) + comparePoint.frequencyComparePoint(password))) / 2): 4;
+        var luds = parseInt(parseInt(ludsPoint.ludsPoint(password).nScore) / 20) < 5? parseInt(parseInt(ludsPoint.ludsPoint(password).nScore) / 20): 4;
+        var levenshteinPoint = parseInt(parseInt(levenshteinDistance.totalLVD(password)) / 5) < 5? parseInt(parseInt(levenshteinDistance.totalLVD(password)) / 5): 4;
+        
+        console.log(zxcvbnPoint + ',' + luds + ',' + levenshteinPoint);
+
+
         if(predictPoint > 0.6) {
             return {
                 password: password,
@@ -22,14 +30,52 @@ class PasswordSecurityCheck {
             };
         }
 
-        var feature = [(koreanZxcvbn(password).score * 2) + comparePoint.frequencyComparePoint(password), ludsPoint.ludsPoint(password).nScore, levenshteinDistance.totalLVD(password)];
+        // var feature = [(koreanZxcvbn(password).score * 2) + comparePoint.frequencyComparePoint(password), ludsPoint.ludsPoint(password).nScore, levenshteinDistance.totalLVD(password)];
 
         var comment = [];
         var recommended = [];
 
-        // feedback
 
+
+        // feedback
+        // 현재는 유출된 비밀번호면랜덤 위치에 랜덤 문자열 추가
+        var arr = [zxcvbnPoint, luds, levenshteinPoint];
+        var check = arr.indexOf(Math.min.apply(null, arr));
+
+        if(check == 0) {
+            comment[0] = '사전의 단어를 사용하였으므로, 단어 변경 요망';
+        }
+        else if(check == 1) {
+            comment[0] = '대문자, 소문자, 숫자, 특수기호를 적절하게 사용하지 않음';
+        }
+        else {
+            comment[0] = '사전의 단어와 비슷한 단어를 사용함';
+        }
+
+
+        var chars = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*()"];
+
+        var length = password.length;
+        var random = Math.floor(Math.random() * (password.length + 1));
+    
+        if(random == length) {
+            var ran = Math.floor(Math.random() * 4);
+    
+            var rnum = Math.floor(Math.random() * chars[ran].length);
+            rnum = chars[ran].substring(rnum, rnum + 1);
+    
+            recommended[0] = password + rnum;
+        }
+        else {
+            var ran = Math.floor(Math.random() * 4);
+    
+            var rnum = Math.floor(Math.random() * chars[ran].length);
+            rnum = chars[ran].substring(rnum, rnum + 1);
         
+            recommended[0] = password.substring(0, random) + rnum + password.substring(random, password.length);
+        }
+
+
 
         return {
             password: password,
